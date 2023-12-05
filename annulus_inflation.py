f4e9ec588a22fe800db7026e79ff5c0e4427ccf2
Liam Murray <murrayla@student.unimelb.edu.au>
2023-12-06 09:41:32 +1100

    annulus_inflation-v2-1-2023DEC06
    Introducing Mooney Rivlin with partial differentiation for Piola

--- annulus_inflation.py
+++ annulus_inflation.py
@@ -141,19 +141,21 @@ def main(test_name, test_type, test_order, refine_check):
     # += Base Dirichlet BCs but broken per subspace
     #    (1): Interpolation space
     #    (2): Internal function to determine if on z = 0
-    # base_dofs_x = fem.locate_dofs_topological(V.sub(0), facet_tag.dim, facet_tag.find(1))
-    # base_dofs_y = fem.locate_dofs_topological(V.sub(1), facet_tag.dim, facet_tag.find(1))
+    base_dofs_x = fem.locate_dofs_topological(V.sub(0), facet_tag.dim, facet_tag.find(1))
+    base_dofs_y = fem.locate_dofs_topological(V.sub(1), facet_tag.dim, facet_tag.find(1))
     base_dofs_z = fem.locate_dofs_topological(V.sub(2), facet_tag.dim, facet_tag.find(1))
     # += Set Dirichlet BCs of (1) on (2)
-    bc_base = fem.dirichletbc(default_scalar_type(0), base_dofs_z, V.sub(2))
+    bc_base_x = fem.dirichletbc(default_scalar_type(0), base_dofs_x, V.sub(0))
+    bc_base_y = fem.dirichletbc(default_scalar_type(0), base_dofs_y, V.sub(1))
+    bc_base_z = fem.dirichletbc(default_scalar_type(0), base_dofs_z, V.sub(2))
     # += Top Dirichlet BCs
     #    (1): Interpolation space
     #    (2): Internal function to determine if on z = 0
     top_dofs_z = fem.locate_dofs_topological(V.sub(2), facet_tag.dim, facet_tag.find(2))
     # += Set Dirichlet BCs of (1) on (2)
-    bc_top = fem.dirichletbc(default_scalar_type(0.2), top_dofs_z, V.sub(2))
+    bc_top_z = fem.dirichletbc(default_scalar_type(0.2), top_dofs_z, V.sub(2))
     # += Concatenate boundaries
-    bc = [bc_base, bc_top]
+    bc = [bc_base_x, bc_base_y, bc_base_z, bc_top_z]
 
     # # +==+==+ 
     # # Pressure Setup
@@ -185,25 +187,24 @@ def main(test_name, test_type, test_order, refine_check):
     # += Invariants
     #    (1): λ1^2 + λ2^2 + λ3^2; tr(C)
     Ic = ufl.variable(ufl.tr(C))
+    # uniMod_Ic = ufl.variable(J**(-2/3) * Ic)
     #    (2): λ1^2*λ2^2 + λ2^2*λ3^2 + λ3^2*λ1^2; 0.5*[(tr(C)^2 - tr(C^2)]
     IIc = ufl.variable((Ic**2 - ufl.inner(C,C))/2)
+    # uniMod_IIc = ufl.variable(J**(-4/3) * IIc)
     #    (3): λ1^2*λ2^2*λ3^2; det(C) = J^2
     J = ufl.variable(ufl.det(F))
     IIIc = ufl.variable(ufl.det(C))
     # += Material Parameters
-    E = default_scalar_type(1.0e4)
-    nu = default_scalar_type(0.3)
-    #    (1): μ = E/(2*(1+v))
-    mu = fem.Constant(domain, E / (2 * (1 + nu)))
-    #    (2): λ = E * v/((1+v)(1-2v))
-    lmbda = fem.Constant(domain, E * nu / ((1 + nu) * (1 - 2 * nu)))
-    # += Strain Energy Density (compressible neo-Hookean model)
-    #    (1): ψ = μ/2 * (Ic - 2) - μ * log(J) + λ/2 * log(J)^2
-    # psi = (mu / 2) * (Ic - 3) - mu * ufl.ln(J) + (lmbda / 2) * (ufl.ln(J))**2
-    psi = 2 * (Ic*J**(-2/3)-3) + 6 *(IIc*J**(-4/3)-3)
+    c1 = 2
+    c2 = 6
+    # += Mooney-Rivlin Strain Energy Density Function
+    psi = c1 * (Ic - 3) + c2 *(IIc - 3)
+    # Terms
+    gamma1 = ufl.diff(psi, Ic) + Ic * ufl.diff(psi, IIc)
+    gamma2 = -ufl.diff(psi, IIc)
     # += First Piola Stress
-    piola = ufl.diff(psi, F)
-
+    # piola = ufl.diff(psi, F)
+    piola = 2 * F * (gamma1*I + gamma2*C) - J*60*ufl.inv(F).T
 
     # +==+==+
     # Setup Variational Problem Solver
@@ -213,7 +214,7 @@ def main(test_name, test_type, test_order, refine_check):
     ds = ufl.Measure('ds', domain=domain, subdomain_data=facet_tag, metadata=metadata)
     dx = ufl.Measure("dx", domain=domain, metadata=metadata)
     # += Residual Equation (Variational, for solving)
-    R = ufl.inner(ufl.grad(v), piola) * dx - ufl.inner(v, B) * dx - ufl.inner(v, T) * ds(2)
+    R = ufl.inner(ufl.grad(v), piola) * dx #- ufl.inner(v, B) * dx # - ufl.inner(v, T) * ds(2)
     # += Problem Setup and Solver
     problem = NonlinearProblem(R, u, bc)
     solver = NewtonSolver(domain.comm, problem)
