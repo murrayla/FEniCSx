b3461d7fedeaa62f38f402b32410b15f9501dbe7
Liam Murray <murrayla@student.unimelb.edu.au>
2023-12-05 15:57:34 +1100

    annulus_inflation-v2-1-2023DEC05
    Implementing variational formulae

--- annulus_inflation.py
+++ annulus_inflation.py
@@ -14,7 +14,9 @@
 # Setup
 # += Imports
 from dolfinx import mesh, fem, io, default_scalar_type
-from dolfinx.fem.petsc import LinearProblem
+from dolfinx.fem.petsc import NonlinearProblem
+from dolfinx.nls.petsc import NewtonSolver
+from numpy import linalg as LNG
 from mpi4py import MPI
 import numpy as np
 import argparse
@@ -95,24 +97,51 @@ def main(test_name, test_type, test_order, refine_check):
     #    (2): Multiprocessing assignment
     #    (3): Rank of multiprocessing
     #    (4): Dimension of mesh
-    domain, _, _ = io.gmshio.read_from_msh("gmsh_msh/testCone.msh", MPI.COMM_WORLD, 0, gdim=MESH_DIM)
+    domain, _, facet_markers = io.gmshio.read_from_msh("gmsh_msh/testCone.msh", MPI.COMM_WORLD, 0, gdim=MESH_DIM)
     # += Create Vector Element
     #    (1): Interpolation style
     #    (2): Cell from mesh domain
     #    (3): Degree of interpolation style
-    fe_interp = ufl.VectorElement("Lagrange", domain.ufl_cell(), degree=MESH_DIM-1)  
+    V = ufl.VectorElement("Lagrange", domain.ufl_cell(), degree=MESH_DIM-1)  
     # += Create Function Space
     #    (1): Mesh domain space
     #    (2): Finite element setup
     # += Geometry Space
-    func_space = fem.FunctionSpace(domain, fe_interp)
+    func_space = fem.FunctionSpace(domain, V)
+
+    # +==+==+
+    # Determine Boundaries
+    #    (1): Base
+    base_facets = mesh.locate_entities_boundary(domain, MESH_DIM-1, lambda x: np.isclose(x[2], 0))
+    #    (2): Top
+    top_facets = mesh.locate_entities_boundary(domain, MESH_DIM-1, lambda x: np.isclose(x[2], LEN_Z))
+    #    (3): Inner Cylinder Surface
+    def inner_bound(x):
+        return np.isclose(np.sqrt(x[0]**2+x[1]**2), R0)
+    inner_facets = mesh.locate_entities_boundary(domain, MESH_DIM-1, inner_bound)
+    #    (4): Outer Cylinder Surface
+    def outer_bound(x):
+        return np.isclose(np.sqrt(x[0]**2+x[1]**2), R1)
+    outer_facets = mesh.locate_entities_boundary(domain, MESH_DIM-1, outer_bound)
+    # += Collate Marked boundaries
+    marked_facets = np.hstack([base_facets, top_facets, inner_facets, outer_facets])
+    # += Assign boundaries IDs
+    marked_values = np.hstack([
+        np.full_like(base_facets, 1), 
+        np.full_like(top_facets, 2),
+        np.full_like(inner_facets, 3), 
+        np.full_like(outer_facets, 4)
+    ])
+    # += Sort and assign
+    sorted_facets = np.argsort(marked_facets)
+    facet_tag = mesh.meshtags(domain, MESH_DIM-1, marked_facets[sorted_facets], marked_values[sorted_facets])
 
     # +==+==+
     # Boundary Conditions
     # += Base Dirichlet BCs
     #    (1): Interpolation space
     #    (2): Internal function to determine if on z = 0
-    base_dofs = fem.locate_dofs_geometrical(func_space, lambda x: np.isclose(x[2], 0))
+    base_dofs =  fem.locate_dofs_topological(func_space, facet_tag.dim, facet_tag.find(1))
     # += Set values at the base boundary
     u_base = fem.Function(func_space)
     u_base.interpolate(lambda x: 0)
@@ -130,16 +159,70 @@ def main(test_name, test_type, test_order, refine_check):
     # += Concatenate boundaries
     bc = [bc_base, bc_top]
 
-    # # +==+==+ 
-    # # Pressure Setup
-    # # += Pressure expression, contribution of pressure at internal radius
-    # p = ufl.exp(('p*x[0]/R', 'p*x[1]/R'), R = R0)
-    # # += Pressure Space (decreasing order of interpolation by 1) 
-    # pre_interp = fem.FunctionSpace(domain, ("Lagrange", test_order-1))
-    # pressure = fem.Function(pre_interp)
-    # # +=  Interpolate expression over points
-    # pre_expr = fem.Expression(p, pre_interp.element.interpolation_points())
-    # pressure.interpolate(pre_expr)
+    # +==+==+ 
+    # Pressure Setup
+    # += Pressure expression, contribution of pressure at internal radius
+    p = ufl.exp(('p*x[0]/R', 'p*x[1]/R'), R = R0)
+    # += Pressure Space (decreasing order of interpolation by 1) 
+    pre_interp = fem.FunctionSpace(domain, ("Lagrange", test_order-1))
+    pressure = fem.Function(pre_interp)
+    # +=  Interpolate expression over points
+    pre_expr = fem.Expression(p, pre_interp.element.interpolation_points())
+    pressure.interpolate(pre_expr)
+
+    # +==+==+
+    # Setup Parameteres for Variational Equation
+    # += Body Forces
+    B = fem.Constant(domain, default_scalar_type((0, 0, 0)))
+    # += Traction Forces (nominal Piola-Kirchoff)
+    T = fem.Constant(domain, default_scalar_type((0, 0, 0)))
+    # += Test and Trial Functions
+    v = ufl.TestFunction(V)
+    u = fem.Function(V)
+    # += Identity Tensor
+    I = ufl.variable(ufl.Identity(MESH_DIM))
+    # += Deformation Gradient Tensor
+    #    F = ∂u/∂X + I
+    F = ufl.variable(I + ufl.grad(u))
+    # += Right Cauchy-Green Tensor
+    C = ufl.variable(F.T * F)
+    # += Invariants
+    #    (1): λ1^2 + λ2^2 + λ3^2; tr(C)
+    Ic = ufl.variable(ufl.tr(C))
+    #    (2): λ1^2*λ2^2 + λ2^2*λ3^2 + λ3^2*λ1^2; 0.5*[(tr(C)^2 - tr(C^2)]
+    IIc = ufl.variable(0.5 * [(ufl.tr(C)**2 - ufl.tr(C**2))])
+    #    (3): λ1^2*λ2^2*λ3^2; det(C) = J^2
+    J = ufl.variable(ufl.det(F))
+    # += Material Parameters
+    E = default_scalar_type(1.0e4)
+    nu = default_scalar_type(0.3)
+    #    (1): μ = E/(2*(1+v))
+    mu = fem.Constant(domain, E / (2 * (1 + nu)))
+    #    (2): λ = E * v/((1+v)(1-2v))
+    lmbda = fem.Constant(domain, E * nu / ((1 + nu) * (1 - 2 * nu)))
+    # += Strain Energy Density (compressible neo-Hookean model)
+    #    (1): ψ = μ/2 * (Ic - 2) - μ * log(J) + λ/2 * log(J)^2
+    psi = (mu / 2) * (Ic - 3) - mu * ufl.ln(J) + (lmbda / 2) * (ufl.ln(J))**2
+    # += First Piola Stress
+    piola = ufl.diff(psi, F)
+
+    # +==+==+
+    # Setup Variational Problem Solver
+    # += Gaussian Quadrature
+    metadata = {"quadrature_degree": 4}
+    # += Domains of integration
+    ds = ufl.Measure('ds', domain=domain, subdomain_data=facet_tag, metadata=metadata)
+    dx = ufl.Measure("dx", domain=domain, metadata=metadata)
+    # += Residual Equation (Variational, for solving)
+    R = ufl.inner(ufl.grad(v), P) * dx - ufl.inner(v, B) * dx - ufl.inner(v, T) * ds(2)
+    # += Problem Setup and Solver
+    problem = NonlinearProblem(R, u, bc)
+    solver = NewtonSolver(domain.comm, problem)
+    # += Tolerances for convergence
+    solver.atol = 1e-8
+    solver.rtol = 1e-8
+    # += Convergence criteria
+    solver.convergence_criterion = "incremental"
 
 # +==+==+
 # Main check for script operation.
