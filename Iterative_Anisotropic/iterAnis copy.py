4d2fce5bb3c5904cd321620877a383f77e74e8f9
Liam Murray <murrayla@student.unimelb.edu.au>
2024-01-17 14:55:09 +1100

    iterAnis-v1-2-20240117

--- Iterative_Anisotropic/iterAnis copy.py
+++ Iterative_Anisotropic/iterAnis copy.py
@@ -24,7 +24,6 @@ from dolfinx.fem.petsc import (
     create_vector, set_bc
 )
 from dolfinx.mesh import (create_unit_cube, CellType, locate_entities_boundary, meshtags)
-from dolfinx.nls.petsc import NewtonSolver
 from petsc4py import PETSc
 from mpi4py import MPI
 #
@@ -44,6 +43,37 @@ FACET_TAGS = {"x0": 1, "x1": 2, "y0": 3, "y1": 4, "z0": 5, "z1": 6, "area": 7}
 # Guccione
 GCC_CONS = [0.5, 10, 1, 1]
 
+class NonlinearPDE_SNESProblem:
+    def __init__(self, F, u, bc):
+        V = u.function_space
+        du = ufl.TrialFunction(V)
+        self.L = form(F)
+        self.a = form(ufl.derivative(F, u, du))
+        self.bc = bc
+        self._F, self._J = None, None
+        self.u = u
+
+    def F(self, snes, x, F):
+        """Assemble residual vector."""
+        x.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                      mode=PETSc.ScatterMode.FORWARD)
+        x.copy(self.u.vector)
+        self.u.vector.ghostUpdate(
+            addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+
+        with F.localForm() as f_local:
+            f_local.set(0.0)
+        assemble_vector(F, self.L)
+        apply_lifting(F, [self.a], bcs=[[self.bc]], x0=[x], scale=-1.0)
+        F.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+        set_bc(F, [self.bc], x, -1.0)
+
+    def J(self, snes, x, J, P):
+        """Assemble Jacobian matrix."""
+        J.zeroEntries()
+        assemble_matrix(J, self.a, bcs=[self.bc])
+        J.assemble()
+
 # +==+==+==+
 # main()
 #   Inputs: 
@@ -59,7 +89,8 @@ def main(test_name, elem_order, quad_order):
     Vp = ufl.FiniteElement("CG", domain.ufl_cell(), degree=elem_order-1)  
     W = FunctionSpace(domain, ufl.MixedElement([Ve, Vp]))
     w = Function(W)
-    dw = Function(W)
+    w0 = Function(W)
+    # w.x.array[:] = 0.0
 
     # +==+==+ 
     # Extract subdomains for dofs
@@ -94,11 +125,36 @@ def main(test_name, elem_order, quad_order):
     sfacets = np.argsort(mfacets)
     ft = meshtags(mesh=domain, dim=fdim, entities=mfacets[sfacets], values=mvalues[sfacets])
 
+    # +==+==+ 
+    # Boundary Conditions
+    # +==+ [x0]
+    x0_dofs_x = locate_dofs_topological((W.sub(0).sub(X), Vx), ft.dim, x0_facets)
+    u0_bc_x = Function(Vx)
+    u0_bc_x.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
+    bc_x0_x = dirichletbc(u0_bc_x, x0_dofs_x, W.sub(0).sub(X))
+    # +==+ [x1]
+    x1_dofs_x = locate_dofs_topological((W.sub(0).sub(X), Vx), ft.dim, x1_facets)
+    u1_bc_x = Function(Vx)
+    u1_bc_x.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(LAMBDA)))
+    bc_x1_x = dirichletbc(u1_bc_x, x1_dofs_x, W.sub(0).sub(X))
+    # +==+ [y0]
+    y0_dofs_y = locate_dofs_topological((W.sub(0).sub(Y), Vy), ft.dim, y0_facets)
+    u0_bc_y = Function(Vy)
+    u0_bc_y.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
+    bc_y0_y = dirichletbc(u0_bc_y, y0_dofs_y, W.sub(0).sub(Y))
+    # +==+ [z0]
+    z0_dofs_z = locate_dofs_topological((W.sub(0).sub(Z), Vz), ft.dim, z0_facets)
+    u0_bc_z = Function(Vz)
+    u0_bc_z.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
+    bc_z0_z = dirichletbc(u0_bc_z, z0_dofs_z, W.sub(0).sub(Z))
+    # +==+ BC Concatenate
+    bc = [bc_x0_x, bc_x1_x, bc_y0_y, bc_z0_z]
+
     # +==+==+
     # Variational Problem Setup
     # += Test and Trial Parameters
     u, p = ufl.split(w)
-    du, dp = ufl.split(dw)
+    u0, p0 = ufl.split(w0)
     v, q = ufl.TestFunctions(W)
     # += Tensor Indices
     i, j, k, a, b, c, d = ufl.indices(7)
@@ -161,113 +217,109 @@ def main(test_name, elem_order, quad_order):
     dx = ufl.Measure("dx", domain=domain, metadata=metadata)
     term = ufl.as_tensor(piola[a, b] * F[j, b] * covDev[j, a])
     R = term * dx + q * (J - 1) * dx 
-
-    # +==+==+
-    # Problem Solver
-    # += Residual Equation Integral
-    #    (0): Quadrature order
-    #    (1): Integration domains
-    #    (2): Residual equation
-    metadata = {"quadrature_degree": quad_order}
-    ds = ufl.Measure('ds', domain=domain, subdomain_data=ft, metadata=metadata)
-    dx = ufl.Measure("dx", domain=domain, metadata=metadata)
-    term = ufl.as_tensor(piola[a, b] * F[j, b] * covDev[j, a])
-    R = term * dx + q * (J - 1) * dx 
     
-    
-    # +==+==+ 
-    # Boundary Conditions
-    # +==+ [x0]
-    x0_dofs_x = locate_dofs_topological((W.sub(0).sub(X), Vx), ft.dim, x0_facets)
-    u0_bc_x = Function(Vx)
-    u0_bc_x.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
-    bc_x0_x = dirichletbc(u0_bc_x, x0_dofs_x, W.sub(0).sub(X))
-    # +==+ [x1]
-    x1_dofs_x = locate_dofs_topological((W.sub(0).sub(X), Vx), ft.dim, x1_facets)
-    u1_bc_x = Function(Vx)
-    u1_bc_x.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(LAMBDA)))
-    bc_x1_x = dirichletbc(u1_bc_x, x1_dofs_x, W.sub(0).sub(X))
-    # +==+ [y0]
-    y0_dofs_y = locate_dofs_topological((W.sub(0).sub(Y), Vy), ft.dim, y0_facets)
-    u0_bc_y = Function(Vy)
-    u0_bc_y.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
-    bc_y0_y = dirichletbc(u0_bc_y, y0_dofs_y, W.sub(0).sub(Y))
-    # +==+ [z0]
-    z0_dofs_z = locate_dofs_topological((W.sub(0).sub(Z), Vz), ft.dim, z0_facets)
-    u0_bc_z = Function(Vz)
-    u0_bc_z.interpolate(lambda x: np.full(x.shape[1], default_scalar_type(0.0)))
-    bc_z0_z = dirichletbc(u0_bc_z, z0_dofs_z, W.sub(0).sub(Z))
-    # +==+ BC Concatenate
-    bc = [bc_x0_x, bc_x1_x, bc_y0_y, bc_z0_z]
-    
-    # += Nonlinear Solver
-    problem = NonlinearProblem(R, w, bc)
-    solver = NewtonSolver(domain.comm, problem)
-    solver.atol = 1e-8
-    solver.rtol = 1e-8
-    solver.convergence_criterion = "incremental"
+    problem = NonlinearPDE_SNESProblem(R, w, bc)
+    w.x.scatter_forward()
+    b = la.create_petsc_vector(W.dofmap.index_map, W.dofmap.index_map_bs)
+    newtJac = create_matrix(problem.a)
 
-    # +==+==+
-    # Solution and Output
-    # += Solve
-    num_its, converged = solver.solve(w)
-    if converged:
-        print(f"Converged in {num_its} iterations.")
-    else:
-        print(f"Not converged after {num_its} iterations.")
-    u_sol, p_sol = w.split()
+    # Create Newton solver and solve
+    snes = PETSc.SNES().create()
+    opts = PETSc.Options()
+    opts['snes_monitor'] = None
+    snes.setFromOptions()
+    snes.setType('vinewtonrsls')  # Changed even to vinewtonssls
+    snes.setFunction(problem.F, b)
+    snes.setJacobian(problem.J, newtJac)
+    snes.setTolerances(rtol=1.0e-8, max_it=50)
 
-    u.x.array[:] = u_sol.x.array
-    p.x.array[:] = p_sol.x.array
+    snes.getKSP().setType("preonly")
+    snes.getKSP().setTolerances(rtol=1.0e-8)
+    snes.getKSP().getPC().setType("lu")
 
-    num_its, converged = solver.solve(w)
-    if converged:
-        print(f"Converged in {num_its} iterations.")
-    else:
-        print(f"Not converged after {num_its} iterations.")
-    u_sol, p_sol = w.split()
+    # w0.x.array[:] = w.x.array
 
-    # +==+==+
-    # Interpolate Stress
-    def cauchy(u, p):
-        print(u.x.array)
-        i = ufl.Identity(MESH_DIM)
-        f = i + ufl.grad(u)
-        c = f.T * f
-        e = 0.5 * (c - i)
-        j = ufl.det(f)
-        q = (
-            GCC_CONS[1] * e[0,0]**2 + 
-            GCC_CONS[2] * (e[1,1]**2 + e[2,2]**2 + 2*(e[1,2] + e[2,1])) + 
-            GCC_CONS[3] * (2*e[0,1]*e[1,0] + 2*e[0,2]*e[2,0])
-        )
-        s = GCC_CONS[0]/4 * ufl.exp(q) * ufl.as_matrix([
-            [4*GCC_CONS[1]*e[0,0], 2*GCC_CONS[3]*(e[1,0] + e[0,1]), 2*GCC_CONS[3]*(e[2,0] + e[0,2])],
-            [2*GCC_CONS[3]*(e[0,1] + e[1,0]), 4*GCC_CONS[2]*e[1,1], 2*GCC_CONS[2]*(e[2,1] + e[1,2])],
-            [2*GCC_CONS[3]*(e[0,2] + e[2,0]), 2*GCC_CONS[2]*(e[1,2] + e[2,1]), 4*GCC_CONS[3]*e[2,2]],
-        ]) - p * ufl.inv(c)
-        sig = 1/j * f*s*f.T
-        return sig
-    #    (1): Create Tensor Function Space
-    #    (2): Define expression over points
-    #    (3): Create function variable
-    #    (4): Interpolate
-    TS = FunctionSpace(domain, ("CG", elem_order, (3, 3)))
-    piola_expr = Expression(cauchy(u_sol, p_sol), TS.element.interpolation_points())
-    sig = Function(TS)
-    sig.interpolate(piola_expr)
+    snes.solve(None, w.vector)
 
-    # += Export
-    strains = w.sub(0).collapse()
-    strains.name = "eps"
-    stresses = sig
-    stresses.name = "sig"
-    with io.VTXWriter(MPI.COMM_WORLD, "Iterative_Anisotropic/paraview_bp/" + test_name + "_eps.bp", strains, engine="BP4") as vtx:
-        vtx.write(0.0)
-        vtx.close()
-    with io.VTXWriter(MPI.COMM_WORLD, "Iterative_Anisotropic/paraview_bp/" + test_name + "_sig.bp", stresses, engine="BP4") as vtx:
-        vtx.write(0.0)
-        vtx.close()
+    # file = io.XDMFFile(MPI.COMM_WORLD, "SNES.xdmf", "w")
+    # file.write_mesh(domain)
+    # t = 0
+    # w.x.array[:] = 0.0
+    # w0.x.array[:] = w.x.array
+    # while (t < 5):
+    #     t += 1
+    #     r = snes.solve(None, w.vector)
+    #     # print(f"Step {int(t/dt)}: num iterations: {r[0]}")
+    #     w0.x.array[:] = w.x.array
+    #     file.write_function(w.sub(0), t)
+    # file.close()
+
+    # # R = ufl.inner(covDev[j], piola[a, b] * F[j, b]) * dx + q * (J - 1) * dx 
+    # # += Nonlinear Solver
+    # #    (0): Definition
+    # #    (1): Newton Iterator
+    # #         (0): Tolerances
+    # #         (1): Convergence
+    # # problem = NonlinearProblem(R, w, bc)
+    # # solver = NewtonSolver(domain.comm, problem)
+    # # solver.atol = 1e-8
+    # # solver.rtol = 1e-8
+    # # solver.convergence_criterion = "incremental"
+
+    # # +==+==+
+    # # Solution and Output
+    # # += Solve
+    # #    (0): Iteration and convergence
+    # #    (1): Return data
+    # # num_its, converged = solver.solve(w)
+    # # if converged:
+    # #     print(f"Converged in {num_its} iterations.")
+    # # else:
+    # #     print(f"Not converged after {num_its} iterations.")
+    # # u_sol, p_sol = w.split()
+
+    # # +==+==+
+    # # Interpolate Stress
+    # #    (0): Function for calculating cauchy stress
+    # def cauchy(u, p):
+    #     print(u.x.array)
+    #     i = ufl.Identity(MESH_DIM)
+    #     f = i + ufl.grad(u)
+    #     c = f.T * f
+    #     e = 0.5 * (c - i)
+    #     j = ufl.det(f)
+    #     q = (
+    #         GCC_CONS[1] * e[0,0]**2 + 
+    #         GCC_CONS[2] * (e[1,1]**2 + e[2,2]**2 + 2*(e[1,2] + e[2,1])) + 
+    #         GCC_CONS[3] * (2*e[0,1]*e[1,0] + 2*e[0,2]*e[2,0])
+    #     )
+    #     s = GCC_CONS[0]/4 * ufl.exp(q) * ufl.as_matrix([
+    #         [4*GCC_CONS[1]*e[0,0], 2*GCC_CONS[3]*(e[1,0] + e[0,1]), 2*GCC_CONS[3]*(e[2,0] + e[0,2])],
+    #         [2*GCC_CONS[3]*(e[0,1] + e[1,0]), 4*GCC_CONS[2]*e[1,1], 2*GCC_CONS[2]*(e[2,1] + e[1,2])],
+    #         [2*GCC_CONS[3]*(e[0,2] + e[2,0]), 2*GCC_CONS[2]*(e[1,2] + e[2,1]), 4*GCC_CONS[3]*e[2,2]],
+    #     ]) - p * ufl.inv(c)
+    #     sig = 1/j * f*s*f.T
+    #     return sig
+    # #    (1): Create Tensor Function Space
+    # #    (2): Define expression over points
+    # #    (3): Create function variable
+    # #    (4): Interpolate
+    # TS = FunctionSpace(domain, ("CG", elem_order, (3, 3)))
+    # piola_expr = Expression(cauchy(u_sol, p_sol), TS.element.interpolation_points())
+    # sig = Function(TS)
+    # sig.interpolate(piola_expr)
+
+    # # += Export
+    # strains = w.sub(0).collapse()
+    # strains.name = "eps"
+    # stresses = sig
+    # stresses.name = "sig"
+    # with io.VTXWriter(MPI.COMM_WORLD, "Iterative_Anisotropic/paraview_bp/" + test_name + "_eps.bp", strains, engine="BP4") as vtx:
+    #     vtx.write(0.0)
+    #     vtx.close()
+    # with io.VTXWriter(MPI.COMM_WORLD, "Iterative_Anisotropic/paraview_bp/" + test_name + "_sig.bp", stresses, engine="BP4") as vtx:
+    #     vtx.write(0.0)
+    #     vtx.close()
     
 # +==+==+
 # Main check for script operation.
