967dabc358845c59ed4fb58b81cf274180d32774
Liam Murray <murrayla@student.unimelb.edu.au>
2024-05-06 17:07:28 +1000

    nodeBasedAnis_2D-v1-1-20240506

--- P_Branch_Contraction/nodeBasedAnis_2D.py
+++ P_Branch_Contraction/nodeBasedAnis_2D.py
@@ -35,13 +35,20 @@ X, Y = 0, 1
 X_ELS = 5
 Y_ELS = 5
 ROT = 0 #np.pi/4
-LAMBDA = 0.02 # 10% extension
+LAMBDA = 0.00 # 10% extension
 MAX_ITS = 5
 FACET_TAGS = {"x0": 1, "x1": 2, "y0": 3, "y1": 4, "area": 7}
 GEO_DIM = 2
 BASE_MS = 10
 # Guccione
 GCC_CONS = [0.5, 10, 1, 1]
+# += Group IDs
+CYTOSOL = 1
+MYO_STRAIGHT = 2
+MYO_UPANGLED = 3
+MYO_DNANGLED = 4
+LEFT_SIDE = 5
+RIGHT_SIDE = 6
 
 # +==+==+==+
 # main()
@@ -54,7 +61,7 @@ def main(test_name, elem_order, quad_order):
     # +==+==+
     # Setup problem space
     file = "P_Branch_Contraction/gmsh_msh/" + test_name + ".msh"
-    domain, _, ft = io.gmshio.read_from_msh(file, MPI.COMM_WORLD, 0, gdim=GEO_DIM)
+    domain, ct, ft = io.gmshio.read_from_msh(file, MPI.COMM_WORLD, 0, gdim=GEO_DIM)
     Ve = ufl.VectorElement(family="CG", cell=domain.ufl_cell(), degree=elem_order)
     Vp = ufl.FiniteElement(family="CG", cell=domain.ufl_cell(), degree=elem_order-1)  
     W = FunctionSpace(domain, ufl.MixedElement([Ve, Vp]))
@@ -120,71 +127,112 @@ def main(test_name, elem_order, quad_order):
 
     # += Fibre alignment positions
     def Fibril(x):
-        return ((x[0] <= 1) and (x[0] >= 0) and (x[1] <= 0.6) and (x[1] >= 0.4))
+        return ((x[0] <= 1) & (x[0] >= 0) & (x[1] <= 0.6) & (x[1] >= 0.4))
     def Cytosol(x):
-        return ((x[0] > 1) and (x[0] < 0) and (x[1] > 0.6) and (x[1] < 0.4))
+        return ((x[0] > 1) & (x[0] < 0) & (x[1] > 0.6) & (x[1] < 0.4))
     
-    kappa = Function(Q)
-    cell_myo = locate_entities(domain, domain.topology.dim, Fibril)
-    cell_cyt = locate_entities(domain, domain.topology.dim, Cytosol)
-    kappa.x.array[cell_myo] = np.full_like(cell_myo, 1, dtype=default_scalar_type)
-    kappa.x.array[cell_cyt] = np.full_like(cell_cyt, 0.1, dtype=default_scalar_type)
+    # += Identify the subdomains of interest  
+    str_myo = ct.find(MYO_STRAIGHT)
+    upa_myo = ct.find(MYO_UPANGLED)
+    dwa_myo = ct.find(MYO_DNANGLED)
+    cytosol = ct.find(CYTOSOL)
 
     # +==+==+
     # Variational Problem Setup
     # += Test and Trial Parameters
     u, p = ufl.split(w)
     v, q = ufl.TestFunctions(W)
-    # += Tensor Indices
-    i, j, k, a, b = ufl.indices(5)
-    # += Curvilinear Mapping
-    Push = ufl.as_matrix([
-        [ufl.cos(ROT), -ufl.sin(ROT)],
-        [ufl.sin(ROT), ufl.cos(ROT)]
-    ])
+    
     # += Curvilinear Coordinates
     x = Function(V)
     x.interpolate(lambda x: (x[0], x[1]))
-    x_nu = ufl.inv(Push) * x
-    u_nu = ufl.inv(Push) * u
-    nu = ufl.inv(Push) * (x + u_nu)
-    # += Metric Tensors
-    Z_un = ufl.grad(x_nu).T * ufl.grad(x_nu)
-    Z_co = ufl.grad(nu).T * ufl.grad(nu)
-    Z_ct = ufl.inv(Z_co)
-    # += Christoffel Symbol | Γ^{i}_{j, a}
-    gamma = ufl.as_tensor((
-        0.5 * Z_ct[k, a] * (
-            ufl.grad(Z_co)[a, i, j] + ufl.grad(Z_co)[a, j, i] - ufl.grad(Z_co)[i, j, a]
+
+    def Str_Calc(x, u, p):
+        
+        # += Tensor Indices
+        i, j, k, a, b = ufl.indices(5)
+        # += Curvilinear Mapping
+        Push = ufl.as_matrix([
+            [ufl.cos(ROT), -ufl.sin(ROT)],
+            [ufl.sin(ROT), ufl.cos(ROT)]
+        ])
+        x_nu = ufl.inv(Push) * x
+        u_nu = ufl.inv(Push) * u
+        nu = ufl.inv(Push) * (x + u_nu)
+        # += Metric Tensors
+        Z_un = ufl.grad(x_nu).T * ufl.grad(x_nu)
+        Z_co = ufl.grad(nu).T * ufl.grad(nu)
+        Z_ct = ufl.inv(Z_co)
+        # += Christoffel Symbol | Γ^{i}_{j, a}
+        gamma = ufl.as_tensor((
+            0.5 * Z_ct[k, a] * (
+                ufl.grad(Z_co)[a, i, j] + ufl.grad(Z_co)[a, j, i] - ufl.grad(Z_co)[i, j, a]
+            )
+        ), [k, i, j])
+        # += Covariant Derivative
+        covDev = ufl.grad(v) - ufl.as_tensor(v[k]*gamma[k, i, j], [i, j])
+        # += Kinematics
+        I = ufl.variable(ufl.Identity(MESH_DIM))
+        F = ufl.as_tensor(I[i, j] + ufl.grad(u)[i, j], [i, j]) * Push
+        C = ufl.variable(ufl.as_tensor(F[k, i]*F[k, j], [i, j]))
+        E = ufl.variable(ufl.as_tensor((0.5*(Z_co[i,j] - Z_un[i,j])), [i, j]))
+        J = ufl.variable(ufl.det(F))
+        # += Material Setup | Guccione
+        Q = (
+            GCC_CONS[1] * E[0,0]**2 + 
+            GCC_CONS[2] * (E[1,1]**2) + 
+            GCC_CONS[3] * (2*E[0,1]*E[1,0])
         )
-    ), [k, i, j])
-    # += Covariant Derivative
-    covDev = ufl.grad(v) - ufl.as_tensor(v[k]*gamma[k, i, j], [i, j])
-    # += Kinematics
-    I = ufl.variable(ufl.Identity(MESH_DIM))
-    F = ufl.as_tensor(I[i, j] + ufl.grad(u)[i, j], [i, j]) * Push
-    C = ufl.variable(ufl.as_tensor(F[k, i]*F[k, j], [i, j]))
-    E = ufl.variable(ufl.as_tensor((0.5*(Z_co[i,j] - Z_un[i,j])), [i, j]))
-    J = ufl.variable(ufl.det(F))
-    # += Material Setup | Guccione
-    Q = (
-        GCC_CONS[1] * E[0,0]**2 + 
-        GCC_CONS[2] * (E[1,1]**2) + 
-        GCC_CONS[3] * (2*E[0,1]*E[1,0])
-    )
-    piola = GCC_CONS[0]/4 * ufl.exp(Q) * ufl.as_matrix([
-        [4*GCC_CONS[1]*E[0,0], 2*GCC_CONS[3]*(E[1,0] + E[0,1])],
-        [2*GCC_CONS[3]*(E[0,1] + E[1,0]), 4*GCC_CONS[2]*E[1,1]],
-    ]) - p * Z_un
+        piola = GCC_CONS[0]/4 * ufl.exp(Q) * ufl.as_matrix([
+            [4*GCC_CONS[1]*E[0,0], 2*GCC_CONS[3]*(E[1,0] + E[0,1])],
+            [2*GCC_CONS[3]*(E[0,1] + E[1,0]), 4*GCC_CONS[2]*E[1,1]],
+        ]) - p * Z_un
+        term = ufl.as_tensor(piola[a, b] * F[j, b] * covDev[j, a])
+        return term, J
+
+    def Cyt_Calc(x, u, p, v):
+       
+        # # += Identity Tensor
+        I = ufl.variable(ufl.Identity(MESH_DIM))
+        # += Deformation Gradient Tensor
+        #    F = ∂u/∂X + I
+        F = ufl.variable(I + ufl.grad(u))
+        # += Right Cauchy-Green Tensor
+        C = ufl.variable(F.T * F)
+        # += Invariants
+        #    (1): λ1^2 + λ2^2 + λ3^2; tr(C)
+        Ic = ufl.variable(ufl.tr(C))
+        #    (2): λ1^2*λ2^2 + λ2^2*λ3^2 + λ3^2*λ1^2; 0.5*[(tr(C)^2 - tr(C^2)]
+        IIc = ufl.variable((Ic**2 - ufl.inner(C,C))/2)
+        #    (3): λ1^2*λ2^2*λ3^2; det(C) = J^2
+        J = ufl.variable(ufl.det(F))
+        # IIIc = ufl.variable(ufl.det(C))
+        # += Material Parameters
+        c1 = 2
+        c2 = 6
+        # += Mooney-Rivlin Strain Energy Density Function
+        psi = c1 * (Ic - 3) + c2 *(IIc - 3) 
+        # Terms
+        gamma1 = ufl.diff(psi, Ic) + Ic * ufl.diff(psi, IIc)
+        gamma2 = -ufl.diff(psi, IIc)
+        # += First Piola Stress
+        piola = 2 * F * (gamma1*I + gamma2*C) + p * J * ufl.inv(F).T
+        term = ufl.inner(ufl.grad(v), piola)
+        return piola, J
+
+
+    x.x.array[str_myo], _ = Str_Calc(x.x.array[str_myo], u, p)
+    x.x.array[cytosol], J = Cyt_Calc(x.x.array[cytosol], u, p, v)
     
+
     # +==+==+
     # Problem Solver
     # += Residual Equation Integral
     metadata = {"quadrature_degree": quad_order}
     ds = ufl.Measure('ds', domain=domain, subdomain_data=ft, metadata=metadata)
     dx = ufl.Measure("dx", domain=domain, metadata=metadata)
-    term = ufl.as_tensor(piola[a, b] * F[j, b] * covDev[j, a])
-    R = term * dx + q * (J - 1) * dx 
+    
+    R = x * dx + q * (J - 1) * dx 
 
     # +==+==+
     # Problem Solver
@@ -195,8 +243,7 @@ def main(test_name, elem_order, quad_order):
     metadata = {"quadrature_degree": quad_order}
     ds = ufl.Measure('ds', domain=domain, subdomain_data=ft, metadata=metadata)
     dx = ufl.Measure("dx", domain=domain, metadata=metadata)
-    term = ufl.as_tensor(piola[a, b] * F[j, b] * covDev[j, a])
-    R = term * dx + q * (J - 1) * dx 
+    R = x * dx + q * (J - 1) * dx 
 
     lam = [0]
 
@@ -240,7 +287,7 @@ def main(test_name, elem_order, quad_order):
 if __name__ == '__main__':
     # +==+ Test Parameters
     # += Test name
-    test_name = "QUAD_XX_SINGLE_MIDDLE"
+    test_name = "SUB_QUAD_XX_SINGLE_MIDDLE"
     # += Element order
     elem_order = 2
     # += Quadature Degree
